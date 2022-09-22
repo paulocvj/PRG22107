4faca1a1be1fbb73755993ba752e1825f2ec9808
paulocvj <paulo.cvj@aluno.ifsc.edu.br>
2022-09-22 10:09:21 -0300

    Add Template example

---
+++ Template/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    vector<int> vec;
+    vector<int>::iterator i;
+
+    while (x != -2147483648) {
+        cin >> x;
+        vec.push_back(x);
+    }
+    vec.pop_back();
+
+    for (i = vec.begin(); i != vec.end(); i++)
+        cout << *i << " ";
+    cout << endl;
+
+    cout << "Min element: " << *min_element(vec.begin(), vec.end()) << endl;
+    cout << "Max element: " << *max_element(vec.begin(), vec.end()) << endl;
+
+    sort(vec.begin(), vec.end());
+
+    for (i = vec.begin(); i != vec.end(); i++)
+        cout << *i << " ";
+    cout << endl;
+
+    return 0;
+}
