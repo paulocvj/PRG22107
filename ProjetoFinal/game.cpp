b8c0722b7d8265a4abfb6475cd68477bb6e2a369
Paulo Cezar <paulo.cvj@aluno.ifsc.edu.br>
2022-12-04 22:18:31 -0300

    Added rounds and scores

--- ProjetoFinal/game.cpp
+++ ProjetoFinal/game.cpp
@@ -8,9 +8,23 @@ void Game::start()
 
     Deck deck;
     Dealer dealer;
+    Player player;
     deck.build();
     deck.shuffle();
 
+    for (int i = 0; i < 10; i++) {
+        playRound(deck, player, i);
+    }
+
+    std::cout << "Your final score was: " << player.getPoints() << std::endl;
+}
+
+void Game::playRound(Deck &deck, Player &player, int round){
+    Dealer dealer;
+    int cardReceived;
+    bool result;
+    char box;
+    
     dealer.takeCard(deck, 1);
     dealer.turnBoardCards();
 
@@ -21,8 +35,12 @@ void Game::start()
 
     result = dealer.checkBetween(cardReceived);
     
-    if ((result && box == 'y') || (!result && box == 'n')) std::cout << "You got it!" << std::endl;
+    if ((result && box == 'y') || (!result && box == 'n')) {
+        std::cout << "You got it!" << std::endl;
+        player.addPoints();
+        }
     else std::cout << "Wrong!" << std::endl;
 
     std::cout << "Your card was -> " << cardReceived << std::endl;
+    std::cout << "Your score after round " << round + 1 << " is " << player.getPoints() << std::endl;
 }
